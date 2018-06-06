9f6c2f245c48df2c9eb478401d9b197aab2cc647
Rafael Lima <rafael.lima@univem.edu.br>
2018-06-05 22:23:50 -0300

    change ftp-client to node-ftp

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-var ftpClient = require("ftp-client");
+var Client = require("ftp");
+var fs = require('fs');
+var DIRECTORY = 'd';
 
 config = {
     host: "127.0.0.1",
@@ -7,8 +9,51 @@ config = {
     password: "12345"
 };
 
-client = new ftpClient(config);
+client = new Client();
+client.on('ready', function () {
+    list_files();
+    // client.put('test', 'test.remote-copy.txt', function (err) {
+    //     if (err) throw err;
+    //     client.end();
+    // });
+});
 
-client.connect(function() {
-    console.log("connected to =>", JSON.stringify(config));
-})
+function list_files() {
+    return new Promise(function (resolve, reject) {
+        client.list(function (err, list) {
+            if (err) {
+                reject();
+            }
+            list.forEach(download)
+            resolve(list);
+            client.end();
+        });
+    });
+}
+var current_path = "";
+function download(file) {
+    if (file.type == DIRECTORY) {
+        log("Moving to directory => ", file.name)
+        client.cwd(file.name, function (err, path) {
+            if (err) throw err;
+            current_path = path;
+            list_files();
+            console.log("teste")
+            client.end();
+        });
+    }
+    console.log(file.name)
+    console.log(current_path)
+
+    // client.get(file, function (err, stream) {
+    //     if (err) throw err;
+    //     stream.once('close', function () {
+    //         client.end();
+    //     });
+    //     stream.pipe(fs.createWriteStream('test.local-copy.txt'));
+    // });
+}
+client.connect(config)
+function log(message, concat = "") {
+    console.log(message, concat);
+}
